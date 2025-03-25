c2baffe39188f8c45ba0332c09ebe75d75e66198
Collins <collowmartial@gmail.com>
2025-03-25 12:33:53 +0300

    get element by ID

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//adding event listeners 
+const buttonOne = document.getElementById("buttonOne")
+const buttonTwo = document.getElementById("buttonTwo")
+const buttonThree = document.getElementById("buttonThree")
+const buttonFour = document.getElementById("buttonFour")
+const buttonFive = document.getElementById("buttonFive")
+const buttonSix = document.getElementById("buttonSix")
+const buttonSeven = document.getElementById("buttonSeven")
+const buttonEight = document.getElementById("buttonEight")
+
+buttonOne.addEventListener("click", (event) => {
+  console.log("Booked");
+});
+
