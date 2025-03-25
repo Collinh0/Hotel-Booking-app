fe55db62b7dbb91870786180ffa7188d7b2d9233
Collins <collowmartial@gmail.com>
2025-03-25 12:52:35 +0300

    buttons-addEventLister

--- src/index.js
+++ src/index.js
@@ -8,7 +8,35 @@ const buttonSix = document.getElementById("buttonSix")
 const buttonSeven = document.getElementById("buttonSeven")
 const buttonEight = document.getElementById("buttonEight")
 
+//adding event listeners
 buttonOne.addEventListener("click", (event) => {
-  console.log("Booked");
+  console.log("Booking Sarova White Sands");
 });
 
+buttonTwo.addEventListener("click", (event) => {
+    console.log("Booking Diamonds Malindi")
+})
+
+buttonThree.addEventListener("click", (e) => {
+    console.log("Booking Villa Ameera")
+})
+
+buttonFour.addEventListener("click", (e) => {
+    console.log("Booking English Point Marina")
+})
+
+buttonFive.addEventListener("click", (e) => {
+    console.log("Booking Chale Island")
+})
+
+buttonSix.addEventListener("click", (e) => {
+    console.log("Booking Eliye Springs")
+})
+
+buttonSeven.addEventListener("click", (e) => {
+    console.log("Booking L. Nakuru Lodge")
+})
+
+buttonEight.addEventListener("click", (e) => {
+    console.log("Booking Acacia Premiere")
+})
